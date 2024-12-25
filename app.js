9bb64966037935935545a759373850e6fc2070a9
Elvin251 <emmmdov485@gmail.com>
2024-12-25 20:42:59 +0400

    d

---
+++ app.js
@@ -0,0 +1,9 @@
+// let arr = [2,6,8,9]
+// // arr.forEach(add => {
+// //     console.log(`kvadrati ${add**2}`)
+// // });
+//  let a = arr.map(element => element**2);
+//  console.log(a)
+let arr = [2,8,9,6,7]; 
+let tek =arr.filter(element=> element%2==1)
+console.log(tek)
